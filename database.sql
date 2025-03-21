75e44e6ad845cca7ba8bf39f1069939ba3db2cc9
Karlo Paz <karlopazpory@gmail.com>
2025-03-21 08:35:30 -0600

    initial commit

---
+++ database.sql
@@ -0,0 +1,16 @@
+CREATE TABLE "students" (
+	"expediente" integer PRIMARY KEY NOT NULL,
+	"nombre" varchar(100) NOT NULL
+);
+
+CREATE TABLE "teachers" (
+  "clave" integer PRIMARY KEY NOT NULL,
+  "nombre" varchar(100) NOT NULL,
+  "grado" varchar(60) NOT NULL
+)
+
+ALTER TABLE "class" (
+  "id" integer PRIMARY KEY NOT NULL,
+  "nombreClase" varchar(100) NOT NULL,
+  "semestre" integer NOT NULL
+)
